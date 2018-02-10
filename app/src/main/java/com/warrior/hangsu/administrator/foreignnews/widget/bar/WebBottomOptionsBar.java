b68fef1c91bce6b78bbe2e1036f3e214ce66de5e
warriorWorld <772192594@qq.com>
2018-02-10 17:34:33 +0800

    2.0

--- app/src/main/java/com/warrior/hangsu/administrator/foreignnews/widget/bar/WebBottomOptionsBar.java
+++ app/src/main/java/com/warrior/hangsu/administrator/foreignnews/widget/bar/WebBottomOptionsBar.java
@@ -12,6 +12,7 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.warrior.hangsu.administrator.foreignnews.R;
 import com.warrior.hangsu.administrator.foreignnews.listener.OnWebBottomBarClickListener;
@@ -24,15 +25,35 @@ import com.warrior.hangsu.administrator.foreignnews.utils.DisplayUtil;
  */
 public class WebBottomOptionsBar extends Dialog implements View.OnClickListener {
     private Context context;
-    private RelativeLayout bottomOptionsRL;
-    private ImageView collectIV, collectedIV, shareIV, refreshIV, logoutIV;
+    private LinearLayout addCollectLl;
+    private LinearLayout addCollectedLl;
+    private LinearLayout refreshLl;
+    private LinearLayout optionsLl;
+    private LinearLayout mangaLl;
+    private LinearLayout exitLl;
+    private LinearLayout headLl;
+    private ImageView userHeadIv;
+    private TextView userNameTv;
+    private RelativeLayout closeDwonArrowRl;
+
+    /**
+     * Find the Views in the layout<br />
+     * <br />
+     * Auto-created on 2018-02-10 17:12:02 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+
+    }
+
+
     private OnWebBottomBarClickListener onWebBottomBarClickListener;
     private OnWebBottomBarOptionsClickListener onWebBottomBarOptionsClickListener;
     private OnWebBottomBarLogoutClickListener onWebBottomBarLogoutClickListener;
 
     public WebBottomOptionsBar(Context context) {
-//        super(context);
-        super(context, R.style.CustomDialog);
+        super(context);
+//        super(context, R.style.CustomDialog);
         this.context = context;
     }
 
@@ -51,52 +72,58 @@ public class WebBottomOptionsBar extends Dialog implements View.OnClickListener 
         window.setGravity(Gravity.BOTTOM);
         window.setWindowAnimations(R.style.AnimationDialog);
         window.setBackgroundDrawableResource(android.R.color.transparent);
-        lp.y = DisplayUtil.dip2px(context, 50); // 闁哄倿顣︾紞鍛磾閻㈡洟宕搁幇顓犲灱
+//        lp.y = DisplayUtil.dip2px(context, 50); // 闁哄倿顣︾紞鍛磾閻㈡洟宕搁幇顓犲灱
         window.setAttributes(lp);
     }
 
     private void init() {
-        bottomOptionsRL = (RelativeLayout) findViewById(R.id.bottom_up_options_rl);
-        collectIV = (ImageView) findViewById(R.id.collect_iv);
-        collectedIV = (ImageView) findViewById(R.id.collected_iv);
-        shareIV = (ImageView) findViewById(R.id.share_iv);
-        refreshIV = (ImageView) findViewById(R.id.refresh_iv);
-        logoutIV = (ImageView) findViewById(R.id.logout_iv);
-        collectIV.setOnClickListener(this);
-        collectedIV.setOnClickListener(this);
-        shareIV.setOnClickListener(this);
-        refreshIV.setOnClickListener(this);
-        logoutIV.setOnClickListener(this);
+        addCollectLl = (LinearLayout) findViewById(R.id.add_collect_ll);
+        addCollectedLl = (LinearLayout) findViewById(R.id.add_collected_ll);
+        refreshLl = (LinearLayout) findViewById(R.id.refresh_ll);
+        optionsLl = (LinearLayout) findViewById(R.id.options_ll);
+        mangaLl = (LinearLayout) findViewById(R.id.manga_ll);
+        exitLl = (LinearLayout) findViewById(R.id.exit_ll);
+        headLl = (LinearLayout) findViewById(R.id.head_ll);
+        userHeadIv = (ImageView) findViewById(R.id.user_head_iv);
+        userNameTv = (TextView) findViewById(R.id.user_name_tv);
+        closeDwonArrowRl = (RelativeLayout) findViewById(R.id.close_dialog_rl);
+
+        closeDwonArrowRl.setOnClickListener(this);
+        addCollectLl.setOnClickListener(this);
+        addCollectedLl.setOnClickListener(this);
+        refreshLl.setOnClickListener(this);
+        optionsLl.setOnClickListener(this);
+        mangaLl.setOnClickListener(this);
+        exitLl.setOnClickListener(this);
+        headLl.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.collect_iv:
+            case R.id.add_collect_ll:
                 if (null != onWebBottomBarOptionsClickListener) {
                     onWebBottomBarOptionsClickListener.onCollectClick();
                 }
                 break;
-            case R.id.collected_iv:
+            case R.id.add_collected_ll:
                 if (null != onWebBottomBarOptionsClickListener) {
                     onWebBottomBarOptionsClickListener.onCollectedClick();
                 }
                 break;
-            case R.id.share_iv:
-                if (null != onWebBottomBarOptionsClickListener) {
-                    onWebBottomBarOptionsClickListener.onShareClick();
-                }
-                break;
-            case R.id.refresh_iv:
+            case R.id.refresh_ll:
                 if (null != onWebBottomBarClickListener) {
                     onWebBottomBarClickListener.onRefreshClick();
                 }
                 break;
-            case R.id.logout_iv:
+            case R.id.exit_ll:
                 if (null != onWebBottomBarLogoutClickListener) {
                     onWebBottomBarLogoutClickListener.onLogoutClick();
                 }
+                break;
+            case R.id.close_dialog_rl:
+                dismiss();
                 break;
         }
         dismiss();
